56f7b6c78beb9df13f32d865384518100865075c
manhtdd <manh.td120901@gmail.com>
2024-10-29 13:25:20 +0700

    Create output folder

--- opencv.sh
+++ opencv.sh
@@ -2,4 +2,5 @@
 
 ./swebench/collect/make_repo/make_repo.sh opencv/opencv $1
 cd ./swebench/collect
-./run_get_tasks_pipeline.sh 'opencv/opencv' 'opencv-prs.jsonl' 'opencv-task-instances.jsonl'
+mkdir output
+./run_get_tasks_pipeline.sh 'opencv/opencv' 'output' 'output'
