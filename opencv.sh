2aeae4caaebc8a114e1f8e7e9bdd47e956eb2076
manhtdd <manh.td120901@gmail.com>
2024-10-29 11:25:47 +0700

    Update scripts to collect opencv: change dir

--- opencv.sh
+++ opencv.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 ./swebench/collect/make_repo/make_repo.sh opencv/opencv $1
-./swebench/collect/run_get_tasks_pipeline.sh 'opencv/opencv' 'opencv-prs.jsonl' 'opencv-task-instances.jsonl'
+cd ./swebench/collect
+./run_get_tasks_pipeline.sh 'opencv/opencv' 'opencv-prs.jsonl' 'opencv-task-instances.jsonl'
