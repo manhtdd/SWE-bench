33b030b3b7fa64c588d0d0310df69c353c93e70b
Veil <65228307+auphong2707@users.noreply.github.com>
2024-11-25 12:54:24 +0700

    Update collect.sh

--- collect.sh
+++ collect.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./swebench/collect/make_repo/make_repo.sh $1 $2
+./swebench/collect/make_repo/make_repo.sh $1 $2 $3
 cd ./swebench/collect
 mkdir output
-./run_get_tasks_pipeline.sh $1 'output' 'output'
+./run_get_tasks_pipeline.sh $2 'output' 'output'
